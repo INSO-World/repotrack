17e12f5050d165b55cfef58f00cd9af5ff96c8cd
JakobNovotny <e11800752@student.tuwien.ac.at>
2025-06-12 11:05:29 +0200

    ADD: Credentials with Git Commands

--- repotrack/src/main/java/at/ac/tuwien/student/e11800752/repositorytracker/service/GitService.java
+++ repotrack/src/main/java/at/ac/tuwien/student/e11800752/repositorytracker/service/GitService.java
@@ -1,6 +1,7 @@
 package at.ac.tuwien.student.e11800752.repositorytracker.service;
 
 import at.ac.tuwien.student.e11800752.repositorytracker.exception.ServiceException;
+import org.eclipse.jgit.api.PullResult;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.File;
@@ -16,12 +17,12 @@ public interface GitService {
      * @param pathToLocalRepository the target file location, where the local copy of the repository should be stored
      * @throws ServiceException in case an error happens while cloning the repository
      */
-    public void cloneRepository(String remoteRepositoryUrl, File pathToLocalRepository) throws ServiceException;
+    public void cloneRepository(String remoteRepositoryUrl, File pathToLocalRepository);
 
     /**
      * Pulls all new changes on the remote repository into the local repository
      *
      * @param repository the local copy of the repository
      */
-    public void pullRepository(File repository);
+    public PullResult pullRepository(File repository);
 }
--- repotrack/src/main/java/at/ac/tuwien/student/e11800752/repositorytracker/service/impl/GitServiceImpl.java
+++ repotrack/src/main/java/at/ac/tuwien/student/e11800752/repositorytracker/service/impl/GitServiceImpl.java
@@ -1,10 +1,16 @@
 package at.ac.tuwien.student.e11800752.repositorytracker.service.impl;
 
 import at.ac.tuwien.student.e11800752.repositorytracker.exception.ServiceException;
+import at.ac.tuwien.student.e11800752.repositorytracker.service.AuthenticationService;
 import at.ac.tuwien.student.e11800752.repositorytracker.service.GitService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.errors.*;
+import org.eclipse.jgit.transport.CredentialsProvider;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -12,11 +18,16 @@ import java.io.IOException;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class GitServiceImpl implements GitService {
 
+    private final AuthenticationServiceImpl authService;
+
     @Override
-    public void cloneRepository(String remoteRepositoryUrl, File pathToLocalRepository) throws ServiceException {
+    public void cloneRepository(String remoteRepositoryUrl, File pathToLocalRepository) {
+        log.info("Cloning repository " + remoteRepositoryUrl);
         try (Git git = Git.cloneRepository()
+                .setCredentialsProvider(authService.getGitCredentials())
                 .setURI(remoteRepositoryUrl)
                 .setDirectory(pathToLocalRepository)
                 .setCloneAllBranches(true)
@@ -29,11 +40,14 @@ public class GitServiceImpl implements GitService {
     }
 
     @Override
-    public void pullRepository(File repository) {
+    public PullResult pullRepository(File repository) {
+        log.info("Pulling repository " + repository.getName());
         try (Git git = Git.open(repository)) {
-            git.pull();
+            return git.pull().setCredentialsProvider(authService.getGitCredentials()).call();
         } catch (IOException e) {
-            throw new RuntimeException("An error has occurred while trying to open a local repository: " + e.getMessage(), e);
+            throw new ServiceException("An error has occurred while trying to open a local repository: " + e.getMessage(), e);
+        } catch (GitAPIException e) {
+            throw new ServiceException("An error has occurred while trying to pull from remote: " + e.getMessage(), e);
         }
     }
 }
