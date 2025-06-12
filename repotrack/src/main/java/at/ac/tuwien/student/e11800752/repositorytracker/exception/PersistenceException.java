681cb5b00977a93fe75df9e90daf15bd76fdf364
JakobNovotny <e11800752@student.tuwien.ac.at>
2025-06-12 11:06:11 +0200

    ADD: Persistence Exception

---
+++ repotrack/src/main/java/at/ac/tuwien/student/e11800752/repositorytracker/exception/PersistenceException.java
@@ -0,0 +1,7 @@
+package at.ac.tuwien.student.e11800752.repositorytracker.exception;
+
+public class PersistenceException extends RuntimeException {
+    public PersistenceException(String message) {
+        super(message);
+    }
+}
